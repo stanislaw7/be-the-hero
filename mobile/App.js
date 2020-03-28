455998f6f31c90751dad48c879552b8bb68bd9e0
Bernardo Garcia <breando7@gmail.com>
2020-03-28 11:40:07 -0300

    Estilização da palicação

--- mobile/App.js
+++ mobile/App.js
@@ -1,23 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello OmniStack</Text>
-    </View>
-  );
+  return <Routes />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: 'red',
-  }
-});
---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import * as MailComposer from 'expo-mail-composer'
+import { View, TouchableOpacity, Image, Text, Linking } from 'react-native'
+
+import logoImg from '../../assets/logo.png'
+import styles from './styles'
+
+export default function Detail() {
+  const navigation = useNavigation()
+  const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$ 120,00'
+
+  function navigateBack() {
+    navigation.goBack()
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subjecti: 'Herói do caso: Cadelinha atropelada',
+      recipients: ['brenado7@gmail.com'],
+      body: message,
+    })
+  }
+
+  function sendWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=554988355104&text=${message}`)
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <TouchableOpacity onPress={navigateBack}>
+          <Feather name="arrow-left" size={28} color="#E02041" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+        <Text style={styles.incidentValue}>ONG Teste</Text>
+
+        <Text style={styles.incidentProperty}>CASO:</Text>
+        <Text style={styles.incidentValue}>Caso Teste</Text>
+
+        <Text style={styles.incidentProperty}>VALOR:</Text>
+        <Text style={styles.incidentValue}>R$ 120,00</Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+        <Text style={styles.heroDescription}>Entre em contato</Text>
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
+            <Text style={styles.actionText}>E-mail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
+}
